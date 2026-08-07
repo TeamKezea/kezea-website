ee885228aac88d393dbd8eb461f07efaaf7ae878
TeamKezea <teamkezea@gmail.com>
2026-08-07 15:38:59 +0300

    Add openPlayer and closePlayer functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function openPlayer(name,role,rank,info){
 
+    document.getElementById("playerModal").style.display="flex";
+
+    document.getElementById("playerName").innerHTML=name;
+
+    document.getElementById("playerRole").innerHTML=role;
+
+    document.getElementById("playerRank").innerHTML="Rank: "+rank;
+
+    document.getElementById("playerInfo").innerHTML=info;
+
+}
+
+
+
+function closePlayer(){
+
+    document.getElementById("playerModal").style.display="none";
+
+}
